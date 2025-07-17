4dc71f74e1a472d8a4c8c5379344051c0958d265
Crutcher Dunnavant <crutcher@gmail.com>
2025-07-17 09:53:22 -0700

    rough demo sketch of symbolic builders

--- crates/bimm/src/data/table/mod.rs
+++ crates/bimm/src/data/table/mod.rs
@@ -14,10 +14,82 @@ pub use schema::*;
 mod tests {
     use super::*;
     use indoc::indoc;
-    use serde_json::json;
+    use serde_json::to_value;
 
     #[test]
     fn test_example() {
+        /// TODO: improve relationship between the symbolic builder mechanism,
+        /// used here to define build info; and the column builder machinery,
+        /// used to actually build the columns.
+
+        fn path_to_class_builder(
+            schema: &BimmTableSchema,
+            path_column: &str,
+        ) -> BimmColumnBuildInfo {
+            // TODO: better lookup / type check error handling as utils on schema.
+            let data_type = &schema[path_column].data_type;
+            if data_type.type_name != std::any::type_name::<String>() {
+                panic!(
+                    "Expected column '{}' to be of type String, found '{}'",
+                    path_column, data_type.type_name
+                );
+            }
+
+            BimmColumnBuildInfo::new("path_to_class", &[("source", path_column)])
+        }
+        fn class_name_to_code_builder(
+            schema: &BimmTableSchema,
+            class_name_column: &str,
+        ) -> BimmColumnBuildInfo {
+            let data_type = &schema[class_name_column].data_type;
+            if data_type.type_name != std::any::type_name::<String>() {
+                panic!(
+                    "Expected column '{}' to be of type String, found '{}'",
+                    class_name_column, data_type.type_name
+                );
+            }
+
+            BimmColumnBuildInfo::new("class_code", &[("source", class_name_column)])
+        }
+
+        fn load_image_builder(
+            schema: &BimmTableSchema,
+            path_column: &str,
+        ) -> BimmColumnBuildInfo {
+            let data_type = &schema[path_column].data_type;
+            if data_type.type_name != std::any::type_name::<String>() {
+                panic!(
+                    "Expected column '{}' to be of type String, found '{}'",
+                    path_column, data_type.type_name
+                );
+            }
+
+            BimmColumnBuildInfo::new("load_image", &[("source", path_column)])
+        }
+
+        #[derive(serde::Serialize, serde::Deserialize)]
+        struct ImageAugConfig {
+            blur: f64,
+            brightness: f64,
+        }
+
+        fn image_aug_builder(
+            schema: &BimmTableSchema,
+            raw_image_column: &str,
+            config: ImageAugConfig,
+        ) -> BimmColumnBuildInfo {
+            let data_type = &schema[raw_image_column].data_type;
+            if data_type.type_name != std::any::type_name::<Vec<u8>>() {
+                panic!(
+                    "Expected column '{}' to be of type Vec<u8>, found '{}'",
+                    raw_image_column, data_type.type_name
+                );
+            }
+
+            BimmColumnBuildInfo::new("image_aug", &[("source", raw_image_column)])
+                .with_config(to_value(config).unwrap())
+        }
+
         let mut schema = BimmTableSchema::from_columns(&[
             BimmColumnSchema::new::<String>("path").with_description("path to the image")
         ]);
@@ -35,37 +107,32 @@ mod tests {
         schema.add_column(
             BimmColumnSchema::new::<String>("class_name")
                 .with_description("category class name")
-                .with_build_info(BimmColumnBuildInfo::new(
-                    "path_to_class",
-                    &[("source", "path")],
-                )),
+                .with_build_info(path_to_class_builder(&schema, "path")),
         );
         schema.add_column(
             BimmColumnSchema::new::<u32>("class")
                 .with_description("category class code")
-                .with_build_info(BimmColumnBuildInfo::new(
-                    "class_code",
-                    &[("source", "class_name")],
-                )),
+                .with_build_info(class_name_to_code_builder(&schema, "class_name")),
         );
 
         schema.add_column(
             BimmColumnSchema::new::<Vec<u8>>("raw_image")
                 .with_description("initial image loaded from disk")
-                .with_build_info(BimmColumnBuildInfo::new(
-                    "load_image",
-                    &[("source", "path")],
-                )),
+                .with_build_info(load_image_builder(&schema, "path")),
         );
 
         // same.
         schema.add_column(
             BimmColumnSchema::new::<Vec<u8>>("aug_image")
                 .with_description("augmented image")
-                .with_build_info(
-                    BimmColumnBuildInfo::new("image_aug", &[("source", "raw_image")])
-                        .with_config(json!({"blur": 0.1, "brightness": 0.2})),
-                ),
+                .with_build_info(image_aug_builder(
+                    &schema,
+                    "raw_image",
+                    ImageAugConfig {
+                        blur: 2.0,
+                        brightness: 0.5,
+                    },
+                )),
         );
 
         assert_eq!(
@@ -131,8 +198,8 @@ mod tests {
                           "source": "raw_image"
                         },
                         "config": {
-                          "blur": 0.1,
-                          "brightness": 0.2
+                          "blur": 2.0,
+                          "brightness": 0.5
                         }
                       }
                     }
